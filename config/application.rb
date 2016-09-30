4ba2dd9fa97147d2e8d627812e79d45eb65ed383
Spencer Miskoviak <smiskoviak@gmail.com>
2016-09-30 13:48:52 -0400

    more styling of login and sign up

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Playthisforus
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      "<span class=\"\">#{html_tag}</span>".html_safe
+    }
   end
 end
