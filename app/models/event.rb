509445cded8b71cfa0435b6847766f7c64ba9486
Spencer Miskoviak <smiskoviak@gmail.com>
2016-10-02 17:46:01 -0400

    add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,26 @@
 class Event < ApplicationRecord
+  JOIN_CODE_LENGTH = 8
+
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+
+  has_secure_token :join_code
+
+  # create a random code to join the event
+  before_create :set_join_code
+
+private
+
+  def set_join_code
+    self.join_code = generate_join_code
+  end
+
+  def generate_join_code
+    loop do
+      token = (0...JOIN_CODE_LENGTH).map { ('a'..'z').to_a[rand(26)] }.join
+      break token unless Event.where(join_code: token).exists?
+    end
+  end
 end
