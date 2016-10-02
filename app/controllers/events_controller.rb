9b6416dfda4413a055aeafa253c635a6435b5700
Spencer Miskoviak <smiskoviak@gmail.com>
2016-10-02 18:40:52 -0400

    add ability to delete event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,7 +45,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to current_user, notice: 'Event was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
