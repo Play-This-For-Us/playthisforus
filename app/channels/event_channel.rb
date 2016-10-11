556c61676a1e88457fb944ed4846662515a36017
Marcus Stojcevich <mstojcevich@gmail.com>
2016-10-11 12:08:38 -0400

    Style changes

--- app/channels/event_channel.rb
+++ app/channels/event_channel.rb
@@ -2,8 +2,8 @@ class EventChannel < ApplicationCable::Channel
   def subscribed
     @event = Event.find(params[:id])
 
-    # TODO use user identifier from cookie
-    @user_identifier = ('a'..'z').to_a.shuffle[0, 16].join
+    # TODO: use user identifier from cookie
+    @user_identifier = ('a'..'z').to_a.sample(16).join
 
     temp_stream_name = String(@event) + '|' + @user_identifier
     stream_from temp_stream_name
@@ -25,26 +25,24 @@ class EventChannel < ApplicationCable::Channel
   end
 
   def vote(data)
-    # TODO way to set vote to 0
+    # TODO: way to set vote to 0
 
     user_identifier = @user_identifier
     song_id = data['song']
 
     song = Song.find_by(id: song_id, event: @event)
 
-    unless song == nil
+    unless song.nil?
       vote = Vote.find_by(user_identifier: user_identifier, song: song)
 
-      if vote != nil # if the vote already exists, update the score
-        puts('Vote existed')
+      if vote.nil?
+        Vote.create!(user_identifier: user_identifier, vote: data['upvote'] ? 1 : -1, song: song)
+      else # if the vote already exists, update the score
         if data['upvote']
           vote.upvote
         else
           vote.downvote
         end
-      else
-        puts('vote didnt exist')
-        Vote.create!(user_identifier: user_identifier, vote: data['upvote'] ? 1 : -1, song: song)
       end
 
       EventChannel.broadcast_to(@event, song)
