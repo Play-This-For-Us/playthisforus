ddd85d022856ada800948c59b8cff8e108df1561
Spencer Miskoviak <smiskoviak@gmail.com>
2016-10-13 15:32:00 -0400

    :art: fix code style

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,5 +1,5 @@
-# app/config/initializers/warden_hooks.rb
-Warden::Manager.after_set_user do |user,auth,opts|
+# frozen_string_literal: true
+Warden::Manager.after_set_user do |user, auth, opts|
   scope = opts[:scope]
   auth.cookies.signed["#{scope}.id"] = user.id
 end
