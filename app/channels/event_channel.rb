d2ba14aff7319851eafc58cff3ef6304b8e83956
Spencer Miskoviak <smiskoviak@gmail.com>
2016-10-13 09:47:55 -0400

    fix broadcast - and cleanup

--- app/channels/event_channel.rb
+++ app/channels/event_channel.rb
@@ -3,23 +3,17 @@ class EventChannel < ApplicationCable::Channel
   def subscribed
     @event = Event.find(params[:id])
 
-    puts String(current_user)
     temp_stream_name = String(@event) + '|' + String(current_user)
     stream_from temp_stream_name
 
-    puts "COUNT #{@event.songs.count}"
     @event.songs.each do |song|
-      puts song
-      ActionCable.server.broadcast(@event, song)
+      ActionCable.server.broadcast(temp_stream_named, song)
     end
 
     stream_for @event
-
-    puts current_user
   end
 
   def submit_song(data)
-    puts data
     return if Song.exists?(uri: data['uri'], event: @event)
 
     song = Song.create!(name: data['name'], artist: data['artist'], art: data['art'], duration: data['duration'],
