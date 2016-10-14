fdbc326f6adcb162fcbf6bdd2262277eee0de619
Spencer Miskoviak <smiskoviak@gmail.com>
2016-10-14 10:03:21 -0400

    start on spotify auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
     @events = @user.events
   end
 
+  def spotify
+    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    # Now you can access user's private data, create playlists and much more
+
+    # Access private data
+    spotify_user.country #=> "US"
+    spotify_user.email   #=> "example@email.com"
+  end
+
   private
 
   def auth_user
