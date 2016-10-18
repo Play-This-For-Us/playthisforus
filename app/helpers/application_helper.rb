d54e53649056b54fb7cd8411e8e6ce9dc6b96ed3
Spencer Miskoviak <smiskoviak@gmail.com>
2016-10-18 19:49:48 -0400

    fix navbar and fix greetings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,5 @@
 # frozen_string_literal: true
 module ApplicationHelper
-  def current_time
-    @current_time ||= Time.now.to_i
-  end
-
-  def midnight
-    @midnight ||= Time.now.beginning_of_day.to_i
-  end
-
-  def noon
-    @noon ||= Time.now.middle_of_day.to_i
-  end
-
-  def five_pm
-    @five_pm ||= Time.now.change(hour: 17).to_i
-  end
-
-  def eight_pm
-    @eight_pm ||= Time.now.change(hour: 20).to_i
-  end
-
   def custom_user
     if current_user.spotify_attributes.present? &&
        current_user.spotify_attributes.key?('display_name') &&
@@ -28,16 +8,13 @@ module ApplicationHelper
     end
   end
 
+  def greetings
+    ["Hey", "Hello", "Hi", "Howdy", "Bonjour", "Good day", "Aloha", "Yo",
+    "Namaste", "Howdy-do", "Cheerio", "G'day", "Good day", "Sup", "Salute"]
+  end
+
   def greeting
-    if midnight.upto(noon).include?(current_time)
-      "Good Morning#{custom_user}!"
-    elsif noon.upto(five_pm).include?(current_time)
-      "Good Afternoon#{custom_user}!"
-    elsif five_pm.upto(eight_pm).include?(current_time)
-      "Good Evening#{custom_user}!"
-    elsif eight_pm.upto(midnight + 1.day).include?(current_time)
-      "Good Night#{custom_user}!"
-    end
+    "#{greetings.sample}#{custom_user}!"
   end
 
   def owner?(event)
