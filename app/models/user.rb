b931c3fbf53efb54369ae141af72277ca4a5da0a
Spencer Miskoviak <smiskoviak@gmail.com>
2016-11-09 12:54:43 -0500

    add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,7 @@ class User < ApplicationRecord
   end
 
   def display_name
+    # safely traverse the spotify attributes as there may be fields that don't exist
     self.try(:spotify_attributes).try(:[], 'display_name')
   end
 end
